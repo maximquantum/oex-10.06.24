5fea687b3edeb39448ebc217f21c0783bb4c73a3
maximquantum <maximquantum148@gmail.com>
2024-08-22 16:17:55 +0200

    done 2

--- oex-10.06.24/src/wegennetwerk/Kruispunt.java
+++ oex-10.06.24/src/wegennetwerk/Kruispunt.java
@@ -1,5 +1,54 @@
 package wegennetwerk;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Kruispunt {
+	
+	/**
+	 * @peerObjects
+	 */
+	Set<Weg> vertrekkendewegen = new HashSet<>();
+	
+	/**
+	 * @peerObjects
+	 */
+	Set<Weg> aankomendewegen = new HashSet<>();
+	
+	
+	/**
+	 * @peerObjects
+	 */
+	public Set<Weg> getVertrekkendewegen() {
+		return vertrekkendewegen;
+	}
+
+
+	/**
+	 * @peerObjects
+	 */
+	public Set<Weg> getAankomendewegen() {
+		return aankomendewegen;
+	}
+
+
+
+//	public void setAankomendewegen(Set<Weg> aankomendewegen) {
+//		this.aankomendewegen = aankomendewegen;
+//	}
+//
+//
+//
+//	public void setVertrekkendewegen(Set<Weg> vertrekkendewegen) {
+//		this.vertrekkendewegen = vertrekkendewegen;
+//	}
+
+
+
+	/**
+	 * @post | getVertrekkendewegen().isEmpty()
+	 * @post | getAankomendewegen().isEmpty()
+	 */
+	public Kruispunt() {}
 
 }
